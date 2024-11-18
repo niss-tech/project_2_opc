924d2294a4ec340fb7139bd7fc9f538eb1b255cd
nisrine adamo <nisrineadamo1@gmail.com>
2024-11-18 15:54:43 +0100

    correction_bug_image

--- load.py
+++ load.py
@@ -12,11 +12,17 @@ def save_to_csv(data, output_file):
         writer.writerows(data)
 
 def download_image(image_url, folder='images'):
-    """Télécharge et enregistre une image dans un dossier."""
+    """Télécharge et enregistre une image dans un dossier à l'extérieur du dossier courant."""
+    # Chemin du dossier parent
+    parent_directory = os.path.dirname(os.getcwd())  
+    folder = os.path.join(parent_directory, folder)  # Chemin absolu du dossier "images"
+
     if not os.path.exists(folder):
         os.makedirs(folder)
+        
     image_name = os.path.join(folder, image_url.split('/')[-1])
     response = requests.get(image_url)
     with open(image_name, 'wb') as file:
         file.write(response.content)
+
 
