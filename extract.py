59efdcff5a34f2bd322727ae9bf37523b9a96f37
nisrine adamo <nisrineadamo1@gmail.com>
2024-11-22 11:41:17 +0100

    delete_allbooks_category_and_add_pagination

--- extract.py
+++ extract.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from utils import get_next_page_url
 
 def fetch_page_content(url):
     """Récupère le contenu HTML d'une page."""
@@ -11,24 +12,45 @@ def fetch_page_content(url):
     except requests.exceptions.HTTPError as e:
         print(f"Erreur HTTP {e.response.status_code} pour l'URL : {url}")
         return None
-
+    
 def extract_category_links(base_url):
-    """Extrait les liens des catégories depuis la page principale."""
+    """Extrait les liens des catégories depuis la page principale, sans inclure le lien 'Books'."""
     soup = fetch_page_content(base_url)
     if soup is None:
         return []
 
+    # Sélectionne tous les liens des catégories
     category_links = soup.select('div.side_categories ul li a')
-    return [urljoin(base_url, link['href']) for link in category_links]
+
+    # Exclus le lien dont le texte est "Books", car il regroupe tout les livres du sites"
+    filtered_links = [
+        link for link in category_links 
+        if link.text.strip().lower() != 'books'  # Vérifier le texte
+    ]
+
+    # Construit les URL absolues et les retournent
+    return [urljoin(base_url, link['href']) for link in filtered_links]
+
+
 
 def extract_product_links(category_url):
-    """Extrait les liens des produits pour une catégorie donnée."""
-    soup = fetch_page_content(category_url)
-    if soup is None:
-        return []
+    """Extrait les liens des produits pour une catégorie, avec gestion de la pagination."""
+    product_links = []  
+    current_url = category_url 
 
-    product_links = [
-        urljoin(category_url, a['href'])
-        for a in soup.select('h3 a')
-    ]
+    while current_url:  # Boucle sur toutes les pages de la catégorie
+        soup = fetch_page_content(current_url)  
+        if soup is None:
+            break  # Si la page est inaccessible, on arrête la boucle
+
+        # Récupère les liens des produits sur la page actuelle
+        product_links.extend(
+            urljoin(current_url, a['href'])
+            for a in soup.select('h3 a')
+        )
+
+        # Passe à la page suivante grâce à la fonction de pagination
+        current_url = get_next_page_url(soup, current_url)
+
     return product_links
+
